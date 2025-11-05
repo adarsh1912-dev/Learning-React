52c83b2eefde7397f27e7f8985946bdc160da955
adarsh-dev <aj19122001@gmail.com>
2025-11-05 21:26:17 +0530

    NewsMag project almost done

---
+++ news-mag/src/App.jsx
@@ -0,0 +1,20 @@
+
+import { useState } from "react";
+import Navbar from "./Components/Navbar";
+import NewsBoard from "./Components/NewsBoard";
+
+
+function App() {
+  
+ const [category,setCategory] = useState("general");
+
+  
+  return (
+    <div>
+      <Navbar setCategory={setCategory}></Navbar>
+      <NewsBoard category={category}></NewsBoard>
+    </div>
+  )
+}
+
+export default App;
